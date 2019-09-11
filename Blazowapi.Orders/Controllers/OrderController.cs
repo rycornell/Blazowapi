3d2ea6971442bd81e2e64ed57c8e90676efacc96
Ryan Manns <ryan.manns@imaginexconsulting.com>
2019-09-11 09:27:41 -0400

    Add project files.

---
+++ Blazowapi.Orders/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Blazowapi.Orders.Controllers
+{    
+    [Route("[controller]")]
+    [Authorize]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Order
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
